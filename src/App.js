8d0c16cb660371c25039b294347026d7eb03acc2
Jon McDuffie <bobber321988@gmail.com>
2023-10-18 00:53:32 +0000

    Remove libraything integration

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import { useEffect, useState } from 'react';
 function App() {
   const userId = 'jmcduffie';
   const apiKey = '1727196843';
-  const [books, setBooks] = useState([]);
+  // This originally called the libraryThing api, but they'ce since
+  // shut it down; Just using static data now
+  const [books, setBooks] = useState([
+    { title: 'Hyperion', author: 'Dan Simmons', cover: 'https://openlibrary.org/works/OL1963268W?edition=key%3A/books/OL24961127M' },
+    { title: 'The Graveyard Book', author: 'Neil Gaiman', cover: 'https://openlibrary.org/works/OL679348W?edition=key%3A/books/OL19429647M' },
+    { title: 'Shards of Earth', author: 'Adrian Tchaikovsky', cover: 'https://openlibrary.org/works/OL24481122W?edition=key%3A/books/OL37485654M' },
+    { title: 'The Color of Magic', author: 'Terry Pratchett', cover: 'https://books.google.com/books?id=7YBX8sWHq4sC&dq=The+color+of+magic&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwj8rJjQsP6BAxVHmIkEHRubBegQ6wF6BAgGEAE'},
+    { title: 'The Stone Sky', author: 'N. K. Jemison', cover: 'https://books.google.com/books?id=cmWnDQAAQBAJ&printsec=frontcover&dq=the+stone+sky&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwjNq4nDsP6BAxU3jIkEHYGQBfYQ6wF6BAgGEAE' },
+    { title: 'Leviathan Wakes', author: 'James S.A. Corey', cover: 'https://books.google.com/books?id=yud-foXqGUEC&printsec=frontcover&dq=leviathan+wakes&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwii0M7hsP6BAxVSkYkEHVJ-AcMQ6wF6BAgKEAE' },
+    { title: 'The Way of Kings', author: 'Brandon Sanderson', cover: 'https://books.google.com/books?id=LN_a49HEpmwC&printsec=frontcover&dq=the+way+of+kings&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwiq0-znsP6BAxUBjokEHTAiD7oQ6wF6BAgKEAE' },
+    { title: 'The Blade Itself', author: 'Joe Abercrombie', cover: 'https://books.google.com/books?id=SlizBgAAQBAJ&printsec=frontcover&dq=the+blade+itself&hl=en&newbks=1&newbks_redir=1&sa=X&ved=2ahUKEwiX59rxsP6BAxVtnokEHZo0CkwQ6wF6BAgJEAE' },
+  ]);
 
-  useEffect(() => {
-    fetch(`https://www.librarything.com/api_getdata.php?userid=${userId}&key=${apiKey}&responseType=json`)
-    .then((resp) => resp.json())
-    .then(data => {
-      if (data.books) {
-        setBooks(Object.values(data.books));
-      }
-    });
+  // useEffect(() => {
+  //   fetch(`https://www.librarything.com/api_getdata.php?userid=${userId}&key=${apiKey}&responseType=json`)
+  //   .then((resp) => resp.json())
+  //   .then(data => {
+  //     if (data.books) {
+  //       setBooks(Object.values(data.books));
+  //     }
+  //   });
 
-  }, []);
+  // }, []);
 
   return (
     <div className="App">
@@ -39,7 +50,7 @@ function Book({book}) {
       </div>
       <div>
         <div className="text-xl font-medium text-black">{book.title}</div>
-        <p className="text-gray-500">{book.author_fl}</p>
+        <p className="text-gray-500">{book.author}</p>
       </div>
 
     </div>
